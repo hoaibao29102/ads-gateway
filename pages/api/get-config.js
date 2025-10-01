5050bec117e4de69695bed9be3b08823cbdb6698
hoaibao29102 <hoaibao29102@gmail.com>
2025-10-02 04:21:12 +0700

    Delete pages directory

--- pages/api/get-config.js
+++
@@ -1,6 +0,0 @@
-import { get } from "@vercel/edge-config";
-export default async function handler(_req, res) {
-  const ACTIVE = await get("ACTIVE");
-  const LINKS = await get("LINKS");
-  res.status(200).json({ ACTIVE, LINKS });
-}
